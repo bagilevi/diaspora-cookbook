4dde90ef3541c7f103b0334940c6a3f14cb96d0f
Levente Bagi <levente@picklive.com>
2011-06-04 20:13:53 +0100

    fixed metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "Levente Bagi"
 maintainer_email  "bagilevi@gmail.com"
-license           "Apache 2.0"
+license           "AGPL 3.0"
 description       "Sets up a Diaspora pod"
 version           "0.0.1"
 
@@ -12,7 +12,7 @@ depends "git"
 recipe "diaspora", "Sets up all diaspora services on one node"
 recipe "diaspora::database", "Installs database server (mysql-server)"
 recipe "diaspora::web_backend", "Installs the web app servers (thin)"
-recipe "diaspora::web_fontend", "Install the web front-end (nginx)"
+recipe "diaspora::web_fontend", "Install the web front-end (apache2)"
 
 
 %w{ ubuntu }.each do |os|
